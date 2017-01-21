fc9669457a82f6c2668ad92dcd13b88e8b184c87
Dominika <dominika.krzyszczyk@intel.com>
2017-01-21 18:47:56 +0100

    bugs (subject: level) part 2

--- Ortogami/app/src/main/java/com/example/dominika/ortogami/OrtoActivity.java
+++ Ortogami/app/src/main/java/com/example/dominika/ortogami/OrtoActivity.java
@@ -141,12 +141,10 @@ public class OrtoActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
 
-        if(!answer) {
+        if (!answer) {
             textWords.setText(array[i]);
             Log.d(TAG, "countTime: " + array[i]);
         }
-      else
-            i++;
 
         countDownHandler.postDelayed(countTimeRunnable, 15000);
     }
@@ -212,6 +210,22 @@ public class OrtoActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
 
+        if (scoreP == 4 && scoreN == 1 || scoreP == 5 && scoreN == 0) {
+            i = 0;
+            level = 1;
+            array = getResources().getStringArray(R.array.words_base_orto_hard);
+            textWords.setText(array[i]);
+            Log.d(TAG, "countTime: " + array[i]);
+        } else if (level == 0){
+            i++;
+            array = getResources().getStringArray(R.array.words_base_orto);
+            textWords.setText(array[i]);
+        } else if(level == 1){
+            i++;
+            array = getResources().getStringArray(R.array.words_base_orto_hard);
+            textWords.setText(array[i]);
+        }
+
         switch (level) {
             case 0:
                 array = getResources().getStringArray(R.array.words_base_orto);
@@ -225,35 +239,14 @@ public class OrtoActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         if (answer == true) {
-            i++;
             textWords.setText(array[i]);
             Log.d(TAG, "countTime: " + array[i]);
             textScoreP.setText(scoreP + " PUNKTÓW");
-        } else
+        } else {
             textScoreN.setText(scoreN + " BŁĘDÓW");
-
-        if ((scoreP == 4 && scoreN == 1) || (scoreP == 5 && scoreN == 0)) {
-            i = 0;
-            level = 1;
-            array = getResources().getStringArray(R.array.words_base_orto_hard);
-            textWords.setText(array[i]);
-            Log.d(TAG, "countTime: " + array[i]);
-        } else if (scoreP+scoreN > 5 && scoreN <= 2) {
-            level = 1;
-            array = getResources().getStringArray(R.array.words_base_orto_hard);
-            textWords.setText(array[i]);
-            Log.d(TAG, "countTime: " + array[i]);
         }
     }
 }
 
-    /*public void randomWords() {
-        for (int i = 0; i < 30; i++) {
-            String[] array = getResources().getStringArray(R.array.words_base_orto);
-            String randomStr = array[new Random().nextInt(array.length)];
-            textWords.setText(randomStr);
-            fallDownButonsHandler.postDelayed(this, 25000);
-        }
-    }*/
 
 
